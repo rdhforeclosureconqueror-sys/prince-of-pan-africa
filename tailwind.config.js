19ebc4d5d47ad9ac97cf2b4ad9198bc0484908b7
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-15 10:36:16 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        panred: "#e63946",
+        pangreen: "#06d6a0",
+        panblack: "#0a0a0a",
+        pangold: "#ffcc00"
+      }
+    }
+  },
+  plugins: []
+}
