79fdc9402ec244e33ff1c4dca63a2780aab51aab
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-23 03:19:33 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import Home from './pages/Home'
-import './styles/theme.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/theme.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-)
+);
