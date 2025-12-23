1cb65406b8fd5e56fabfcd52d85894e300f01fe4
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-23 01:30:05 -0600

    Create JournalPage.jsx

---
+++ src/pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/JournalPage.jsx
+import React from "react";
+import JournalPanel from "../components/JournalPanel";
+
+export default function JournalPage() {
+  return (
+    <div style={{ maxWidth: 980, margin: "0 auto", padding: "18px 14px" }}>
+      <JournalPanel />
+    </div>
+  );
+}
