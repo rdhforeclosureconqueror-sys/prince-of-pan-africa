56943ad3e575b9b691b7b7fa48f0ab8a3bdc44cb
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-23 01:28:40 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MufasaShell from "./layouts/MufasaShell";
 import Home from "./pages/Home";
 import PortalDecolonize from "./pages/PortalDecolonize";
+
+import CalendarPage from "./pages/CalendarPage";
+import JournalPage from "./pages/JournalPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portal/decolonize" element={<PortalDecolonize />} />
+        {/* Shell wraps all “main” pages */}
+        <Route element={<MufasaShell />}>
+          <Route index element={<Home />} />
+          <Route path="calendar" element={<CalendarPage />} />
+          <Route path="journal" element={<JournalPage />} />
+          <Route path="portal/decolonize" element={<PortalDecolonize />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
