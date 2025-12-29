3fb07fcd9e31734dfd71113c87a575bd7936eb3c
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-29 00:59:45 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LibraryDecolonize from "./pages/LibraryDecolonize";
-import MembershipPlan from "./pages/MembershipPlan";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import LoginGate from "./components/LoginGate";
 
 import MufasaShell from "./layouts/MufasaShell";
 import Home from "./pages/Home";
-import PortalDecolonize from "./pages/PortalDecolonize";
 import TimelinePage from "./pages/TimelinePage";
 import CalendarPage from "./pages/CalendarPage";
 import JournalPage from "./pages/JournalPage";
+import LibraryDecolonize from "./pages/LibraryDecolonize";
+import MembershipPlan from "./pages/MembershipPlan";
+import PortalDecolonize from "./pages/PortalDecolonize";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Shell wraps all “main” pages */}
-        <Route element={<MufasaShell />}>
-          <Route index element={<Home />} />
-          <Route path="timeline" element={<TimelinePage />} />
-          <Route path="calendar" element={<CalendarPage />} />
-          <Route path="journal" element={<JournalPage />} />
-          <Route path="library" element={<LibraryDecolonize />} />
-          <Route path="membership" element={<MembershipPlan />} />
-          <Route path="portal/decolonize" element={<PortalDecolonize />} />
-        </Route>
-      </Routes>
+      {/* Everything is protected behind the human gate */}
+      <LoginGate>
+        <Routes>
+          {/* Shell wraps all “main” pages */}
+          <Route element={<MufasaShell />}>
+            <Route index element={<Home />} />
+            <Route path="timeline" element={<TimelinePage />} />
+            <Route path="calendar" element={<CalendarPage />} />
+            <Route path="journal" element={<JournalPage />} />
+            <Route path="library" element={<LibraryDecolonize />} />
+            <Route path="membership" element={<MembershipPlan />} />
+            <Route path="portal/decolonize" element={<PortalDecolonize />} />
+
+            {/* Safety: if someone hits a bad URL, send them home */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </LoginGate>
     </BrowserRouter>
   );
 }
