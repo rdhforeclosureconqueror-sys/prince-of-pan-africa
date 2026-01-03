3e628f5a336784e028d47888d6e7144356bf61de
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2026-01-03 17:10:59 -0600

    Create UniverseOverlay.jsx

---
+++ src/components/UniverseOverlay.jsx
@@ -0,0 +1,12 @@
+// src/components/UniverseOverlay.jsx
+import React from "react";
+import "../styles/dashboard.css";
+
+export default function UniverseOverlay() {
+  return (
+    <>
+      <div className="universe-bg" />
+      <div className="universe-overlay" />
+    </>
+  );
+}
