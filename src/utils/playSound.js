b73625086d3817e8040a94e24ccd9e748020d871
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2026-01-05 02:13:02 -0600

    Update playSound.js

--- src/utils/playSound.js
+++ src/utils/playSound.js
@@ -1,6 +1,15 @@
-// ✅ src/utils/playSound.js
+// src/utils/playSound.js
+const motivateSounds = [
+  "/sounds/you_got_this.mp3",
+  "/sounds/keep_pushing.mp3",
+  "/sounds/excellent_work.mp3",
+];
+
 export function playSound(type = "achievement") {
-  const file = type === "motivate" ? "/sounds/motivate.mp3" : "/sounds/achievement.mp3";
+  let file = "/sounds/achievement.mp3";
+  if (type === "motivate") {
+    file = motivateSounds[Math.floor(Math.random() * motivateSounds.length)];
+  }
   const audio = new Audio(file);
   audio.volume = 0.5;
   audio.play().catch(() => {});
