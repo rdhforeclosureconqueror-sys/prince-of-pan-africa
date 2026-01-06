fe6e97554f3469c1ae9a280d2948dff30c246aef
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2026-01-06 04:42:22 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,17 @@
+// ✅ src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/theme.css";
+
+// 🧩 Import all global styles here
+import "./styles/themes.css";
+import "./styles/universe.css";
+import "./styles/MufasaShell.css";
+import "./styles/dashboard.css";
+import "./styles/achievementToast.css";
+import "./styles/xpOverlay.css";
+import "./styles/ledgerV2.css";
+import "./styles/admin.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
